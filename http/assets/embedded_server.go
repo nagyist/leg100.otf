253f7b6b02658c636f30edc46db240a6a10c4a1b
Louis Garman <louisgarman@gmail.com>
2021-11-25 18:22:17 +0000

    wip

--- http/assets/embedded_server.go
+++ http/assets/embedded_server.go
@@ -1,16 +1,36 @@
 package assets
 
 import (
+	"embed"
+	"fmt"
 	"html/template"
+	"io/fs"
 	"net/http"
+	"path/filepath"
 )
 
-// EmbeddedServer embeds assets within the go binary (the default).
+// EmbeddedServer provides access to assets embedded in the go binary.
 type EmbeddedServer struct {
+	// templates maps template names to parsed contents
 	templates map[string]*template.Template
 }
 
-func NewEmbeddedServer() *EmbeddedServer {
+func NewEmbeddedServer(filesystem embed.FS, opts ServerOptions) (*EmbeddedServer, error) {
+	pattern := fmt.Sprintf("%s/*.tmpl", opts.ContentPath)
+	paths, err := fs.Glob(filesystem, pattern)
+	if err != nil {
+		return nil, fmt.Errorf("unable to read embedded templates directory: %w", err)
+	}
+
+	server := EmbeddedServer{
+		templates: make(map[string]*template.Template, len(paths)),
+	}
+
+	for _, p := range paths {
+		contentPath := filepath.Join(contentDir, p.Name())
+
+		templates[p.Name()] = template.Must(template.ParseFS(static, layoutPath, contentPath))
+	}
 	return &EmbeddedServer{templates: templates}
 }
 
